52a7ca2401a19549006465f9b2d22c8860d61ce8
Dave Ray <daveray@gmail.com>
2011-05-09 20:26:24 -0400

    Add lein-clojars to project.clj.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [com.miglayout/miglayout "3.7.4"]]
   :dev-dependencies [[com.stuartsierra/lazytest "1.1.2"]
-                     [lein-autotest "1.1.0"]]
+                     [lein-autotest "1.1.0"]
+                     [lein-clojars "0.6.0"]]
   :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"})
 
