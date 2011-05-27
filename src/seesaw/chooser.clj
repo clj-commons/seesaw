bda23e0f13ac6e7f17f754f997493d7289b23816
Thomas Karolski <Thomas.Karolski@googlemail.com>
2011-05-27 19:24:57 +0200

    - add filter functionality

--- src/seesaw/chooser.clj
+++ src/seesaw/chooser.clj
@@ -21,6 +21,8 @@
 (def ^{:private true} file-chooser-options {
   :dir    #(.setCurrentDirectory %1 %2)
   :multi? #(.setMultiSelectionEnabled %1 (boolean %2))
+  :filters #(doseq [[name exts] %2]
+              (.setFileFilter %1 (javax.swing.filechooser.FileNameExtensionFilter. name (into-array exts))))
 })
 
 (def ^{:private true} last-dir (atom nil))
