7deaff0ec17be1eed569d2f39652a78e5d5a4b1c
Thomas Karolski <Thomas.Karolski@googlemail.com>
2011-05-28 20:57:30 +0200

    - add support for atoms in slider

--- src/seesaw/core.clj
+++ src/seesaw/core.clj
@@ -1383,8 +1383,18 @@
   :orientation #(.setOrientation %1 ({:horizontal javax.swing.SwingConstants/HORIZONTAL
                                       :vertical javax.swing.SwingConstants/VERTICAL} %2
                                       (throw (IllegalArgumentException. ":orientation must be either :horizontal or :vertical."))))
-  :value #(do (check-args (number? %2) ":value must be a number.")
-              (.setValue %1 %2))
+  :value #(cond (isa? (type %2) clojure.lang.Atom)
+                (do (add-watch %2 (keyword (gensym "seesaw-slider-watcher"))
+                               (fn [k r o n] (when (not (= o n))
+                                               (invoke-now (.setValue %1 n)))))
+                    (listen %1 :change (fn [e] (swap! %2
+                                                (fn [o] (if (not (= (.getValue %1) o))
+                                                          (.getValue %1)
+                                                          o))))))
+                (number? %2)
+                (.setValue %1 %2)
+                true
+                (throw (IllegalArgumentException. ":value must be a number or an atom.")))
   :min #(do (check-args (number? %2) ":min must be a number.")
             (.setMinimum %1 %2))
   :max #(do (check-args (number? %2) ":max must be a number.")
@@ -1416,7 +1426,8 @@
   Besides the default options, options can also be one of:
 
     :orientation   The orientation of the slider. One of :horizontal, :vertical.
-    :value         The initial numerical value that is to be set.
+    :value         The initial numerical value that is to be set. This may be a
+                   ref, in which case the ref will be kept in sync with the slider.
     :min           The minimum numerical value which can be set.
     :max           The maximum numericl value which can be set.
     :minor-tick-spacing  The spacing between minor ticks. If set, will also set :paint-ticks? to true.
