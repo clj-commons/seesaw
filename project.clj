8b4aa0b4c7fb83739df7de6f1435acdd16beb0f7
Dave Ray <daveray@gmail.com>
2011-05-30 22:41:47 -0400

    Add link in project description.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject seesaw "1.0.5-SNAPSHOT"
-  :description "A Swing wrapper/DSL for Clojure"
+  :description "A Swing wrapper/DSL for Clojure. See http://seesaw-clj.org for more info."
   :warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [com.miglayout/miglayout "3.7.4"]]
