02c1cab4ea3b741d1b52949d3c78930e1d6ba280
Dave Ray <daveray@gmail.com>
2011-05-30 22:56:41 -0400

    license info

--- project.clj
+++ project.clj
@@ -4,6 +4,10 @@
   :mailing-list {:name "seesaw-clj"
                  :achive "https://groups.google.com/group/seesaw-clj"
                  :post "seesaw-clj@googlegroups.com"}
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [com.miglayout/miglayout "3.7.4"]]
