a63b59eaa04636964f9bb958e991cea3451d085e
Thomas Karolski <Thomas.Karolski@googlemail.com>
2011-06-04 17:15:08 +0200

    - add test for preference atom

--- test/seesaw/test/pref.clj
+++ test/seesaw/test/pref.clj
@@ -13,4 +13,17 @@
   (:use [lazytest.describe :only (describe it testing)]
         [lazytest.expect :only (expect)]))
 
-; TODO add tests here!
+(describe preference-atom
+  (it "should return an atom with nil as its default value"
+    (do (.remove (preferences-node) "key")
+     (let [atom (preference-atom "key")]
+       (expect (= @atom nil)))))
+  (it "should return an atom with the specified default value"
+    (do (.remove (preferences-node) "key")
+        (let [atom (preference-atom "key" 'some-value)]
+          (expect (= @atom 'some-value)))))
+  (it "should keep pref in sync with atom"
+    (do (.remove (preferences-node) "key")
+        (let [atom (preference-atom "key")]
+          (reset! atom 'new-value)
+          (expect (= (read-string (.get (preferences-node) "key" "nil")) 'new-value))))))
