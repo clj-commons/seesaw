4940e7ac41965ce790f1c0546f5e54109d97a006
Meikel Brandmeyer <mb@kotka.de>
2011-07-01 01:22:39 +0200

    Add forms-panel

--- src/seesaw/forms.clj
+++ src/seesaw/forms.clj
@@ -1,7 +1,13 @@
 (ns seesaw.forms
   (:import
     java.awt.Component
-    com.jgoodies.forms.builder.DefaultFormBuilder))
+    javax.swing.JPanel
+    com.jgoodies.forms.builder.DefaultFormBuilder
+    com.jgoodies.forms.layout.FormLayout)
+  (:require
+    seesaw.core)
+  (:use
+    [seesaw.util :only (apply-options)]))
 
 (defprotocol ComponentSpec
   (append [this builder] "Add the given component to the form builder"))
@@ -49,3 +55,40 @@
      ComponentSpec
      (append [this builder]
        (.appendSeparator builder this)))))
+
+(def ^{:private true} builder-options
+  {:items                  #(doseq [item %2] (append item %1))
+   :default-dialog-border? #(when %2 (.setDefaultDialogBorder %1))
+   :default-row-spec       #(.setDefaultRowSpec %1 %2)
+   :leading-column-offset  #(.setLeadingColumnOffset %1 %2)
+   :line-gap-size          #(.setLineGapSize %1 %2)
+   :paragraph-gap-size     #(.setParagraphGapSize %1 %2)})
+
+(defn ^JPanel forms-panel
+  "Construct a panel with a FormLayout. The column spec is
+  expected to be a FormLayout column spec in string form.
+
+  The items are a list of strings, components or any of the
+  combinators. For example:
+
+      :items [\"Login\" (text) next-line
+              \"Password\" (span (text) 3)]
+
+  Takes the following special properties. They correspond
+  to the DefaultFormBuilder option of the same name.
+
+      :default-dialog-border?
+      :default-row-spec
+      :leading-column-offset
+      :line-gap-size
+      :paragraph-gap-size
+
+  See http://www.jgoodies.com/freeware/forms/index.html"
+  {:seesaw {:class `JPanel}}
+  [column-spec & opts]
+  (let [layout  (FormLayout. column-spec "")
+        builder (DefaultFormBuilder. layout)]
+    (apply-options builder opts builder-options)
+    (doto (.getPanel builder)
+      (apply-options opts (merge @#'seesaw.core/default-options
+                                 {:items (fn [x _] x)})))))
