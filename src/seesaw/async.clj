33153e9c8d6f7b3c761ed8adc846fa21ff326ae5
Meikel Brandmeyer <mb@kotka.de>
2011-07-04 23:45:14 +0200

    Add let-async

--- src/seesaw/async.clj
+++ src/seesaw/async.clj
@@ -18,3 +18,31 @@
   "Runs the block asynchronuously and passes on the result to the continuation."
   [& body]
   `(call-async (fn [] ~@body)))
+
+(defmacro let-async
+  "Create a binding of locals similar to let. However on the right hand side
+  of the bindings are asynchronuous functions. Their results will be bound to
+  the locals.
+
+  The locals may be used in later bindings and the body (just as in a let).
+
+  Example:
+
+      (let-async [is-even? (call-async even? 2)
+                  result   (doasync
+                             (if is-even?
+                               (+ 2 1)
+                               (- 2 1)))]
+        (* result 3))
+
+  Executes the steps and the body asynchronuously and passes on the result
+  to the continuation."
+  [steps & body]
+  (let [global-continue (gensym "global-continue")
+        step            (fn [inner [local local-continue]]
+                          `(~local-continue (fn [~local] ~inner)))]
+    `(fn [~global-continue]
+       ~(->> steps
+          reverse
+          (partition 2)
+          (reduce step `(~global-continue (do ~@body)))))))
