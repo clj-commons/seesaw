9fb837cc4fff4e65bee05fb573fad893db1e7e71
Meikel Brandmeyer <mb@kotka.de>
2011-07-04 23:31:41 +0200

    Add run-async

--- src/seesaw/async.clj
+++ src/seesaw/async.clj
@@ -1,2 +1,9 @@
 (ns seesaw.async)
 
+(defn run-async
+  "Run an asynchronuous workflow. Returns a promise which may be dereferenced
+  for the workflow result if there is any."
+  [continue]
+  (let [result (promise)]
+    (continue #(deliver result %))
+    result))
