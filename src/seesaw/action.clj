a7bb399d77ff121fc411fa94f825a71c23917e2a
odyssomay <odyssomay@gmail.com>
2011-07-14 11:35:52 -0700

    convert :mnemonic character to upper case

--- src/seesaw/action.clj
+++ src/seesaw/action.clj
@@ -27,7 +27,7 @@
   :tip       #(.putValue ^Action %1 Action/SHORT_DESCRIPTION (str %2))
   :icon      #(.putValue ^Action %1 Action/SMALL_ICON (icon %2))
   :key       #(.putValue ^Action %1 Action/ACCELERATOR_KEY (keystroke %2))
-  :mnemonic  #(.putValue ^Action %1 Action/MNEMONIC_KEY (int %2))
+  :mnemonic  #(.putValue ^Action %1 Action/MNEMONIC_KEY (int (Character/toUpperCase %2)))
   :handler   #(put-meta! %1 action-handler-property %2)
 })
 
