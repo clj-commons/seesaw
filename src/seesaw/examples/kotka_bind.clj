47c2be4a7a8d9a2e26870d63213a0e84a8c822b1
Dave Ray <daveray@gmail.com>
2011-07-16 22:19:29 -0400

    For fun, ported MB's UI decoupling example to use seesaw.bind. See comments for more info.

---
+++ src/seesaw/examples/kotka_bind.clj
@@ -0,0 +1,67 @@
+;  Copyright (c) Dave Ray, 2011. All rights reserved.
+
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this 
+;   distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
+(ns seesaw.examples.kotka-bind
+  (:require [seesaw.bind :as bind])
+  (:use [seesaw.core]))
+
+; seesaw.bind applied to http://kotka.de/blog/2010/05/Decoupling_Logic_and_GUI.html
+;
+; shows how to bind atom values to widge properties to decouple logic from
+; display.
+
+(defn processing
+  [items done? canceled? progress]
+  (doseq [item (take-while (fn [_] (not @canceled?)) items)]
+    (println item)
+    (Thread/sleep 50)
+    (swap! progress inc))
+  (reset! done? true))
+
+(defn startup
+  []
+  (let [items     (take 500 (iterate inc 0))
+        progress  (atom 0)
+        done?     (atom false)
+        canceled? (atom false)
+        pbar      (progress-bar :value 0 :max (count items))
+        done      (button :text "Done" :enabled? false)
+        cancel    (button :text "Cancel")
+        panel     (border-panel
+                    :border 5 :hgap 5 :vgap 5
+                    :north "The progress bar updates 20 times per second"
+                    :center pbar
+                    :south (grid-panel :rows 1 :items [done cancel]))]
+    ; Wire up the done button
+    (listen done :action-performed (fn [e] (dispose! (to-root e))))
+    ; Toggle buttons on done
+    (bind/bind done? 
+        (bind/some identity) ; (when done?)
+        (bind/tee
+          (bind/property done :enabled?)
+          (bind/bind (bind/transform not) (bind/property cancel :enabled?))))
+    ; Wire up cancel button.
+    (listen cancel :action-performed 
+      (fn [_] 
+        (reset! canceled? true)))
+    ; Wire up progress bar.
+    (bind/bind progress pbar)
+    (-> 
+      (frame :title "Example GUI" :on-close :dispose :content panel)
+      pack! 
+      show!)
+    (-> #(processing items done? canceled? progress) Thread. .start)))
+
+(defn -main
+  []
+  (invoke-now (startup)))
+
+;(-main)
+
