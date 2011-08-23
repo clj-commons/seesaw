159d74a3f1c369bc65922967dd6eebb0c44dad70
Dave Ray <daveray@gmail.com>
2011-08-22 22:26:11 -0400

    updates

--- examples/gaidica/project.clj
+++ examples/gaidica/project.clj
@@ -1,5 +1,5 @@
 (defproject gaidica "1.0.0-SNAPSHOT"
   :description "Example Seesaw application"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [seesaw "1.1.0"]])
