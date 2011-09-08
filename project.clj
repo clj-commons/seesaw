004b0f96067e1587ddd8376524f6d485a822b1c1
Dave Ray <daveray@gmail.com>
2011-09-08 18:53:11 -0400

    1.2.1 snapshot bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject seesaw "1.2.0"
+(defproject seesaw "1.2.1-SNAPSHOT"
   :description "A Swing wrapper/DSL for Clojure. You want seesaw.core, FYI. See http://seesaw-clj.org for more info."
   :url "http://seesaw-clj.org"
   :mailing-list {:name "seesaw-clj"
