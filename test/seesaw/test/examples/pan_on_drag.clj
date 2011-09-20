8092392e71c52a6e185414fc676838eb92b26a3b
Dave Ray <daveray@gmail.com>
2011-09-19 20:37:54 -0400

    more panning stuff

--- test/seesaw/test/examples/pan_on_drag.clj
+++ test/seesaw/test/examples/pan_on_drag.clj
@@ -2,31 +2,53 @@
   (:require [seesaw.core :as sc])
   (:require [seesaw.behave :as behave]))
 
-(defn scrollable-image []
+(defn scrollable-image [id]
   (sc/scrollable
-    (sc/label :icon "file:///Users/dave/Desktop/IMG_0058.JPG")))
+    (sc/label :id id 
+              ;:icon "file:///Users/dave/Desktop/IMG_0058.JPG"
+              )))
 
-(defn pan-on-drag [target]
-  (behave/when-mouse-dragged target
-    :drag (fn [e [dx dy]]
-            (let [^javax.swing.JComponent widget   (sc/to-widget e)
-                  ^javax.swing.JViewport  viewport (.. widget getParent)
-                  rect      (.getViewRect viewport)
-                  full-size (.getViewSize viewport)
-                  [x y w h] [(.x rect) (.y rect) (.width rect) (.height rect)]
-                  new-x (Math/min (Math/max 0 (+ x dx)) (- (.width full-size) w))
-                  new-y (Math/min (Math/max 0 (+ y dy)) (- (.height full-size) h))]
-              (.setViewPosition viewport (java.awt.Point. new-x new-y))))))
+(defn pan [view-to-pan dx dy]
+  (let [^javax.swing.JViewport  viewport (.. view-to-pan getParent)
+        rect      (.getViewRect viewport)
+        full-size (.getViewSize viewport)
+        [x y w h] [(.x rect) (.y rect) (.width rect) (.height rect)]
+        new-x (Math/min (Math/max 0 (+ x (int dx))) (- (.width full-size) w))
+        new-y (Math/min (Math/max 0 (+ y (int dy))) (- (.height full-size) h))]
+    (.setViewPosition viewport (java.awt.Point. new-x new-y))))
+
+(defn- calculate-scales [panner view-to-pan]
+  [(/ (.getWidth view-to-pan) (.getWidth panner))
+   (/ (.getHeight view-to-pan) (.getHeight panner))])
+
+(defn pan-on-drag 
+  ([view-to-pan & {:keys [panner speed] :or {panner view-to-pan speed 1.0}}]
+    (behave/when-mouse-dragged panner 
+      :drag (fn [e [dx dy]] 
+              (let [[sx sy] (calculate-scales panner view-to-pan)] 
+                (pan view-to-pan (* dx sx speed) (* dy sy speed)))))))
 
 (defn add-behaviors [root]
-  (pan-on-drag (first (sc/select root [:JLabel])))
+  (pan-on-drag (sc/select root [:#image]))
+  (pan-on-drag (sc/select root [:#image]) 
+               :panner (sc/select root [:#panner]) 
+               :scale 5.0)
   root)
 
 (defn app []
   (sc/frame
     :title "Pan on drag"
     :size [480 :by 480]
-    :content (sc/border-panel :center (scrollable-image))))
+    :content 
+     (sc/border-panel 
+       :north  "Click and drag to pan the image"
+       :center (scrollable-image :image)
+       :south  (sc/flow-panel 
+                 :items ["Or drag on the blue field to pan the image"
+                        (sc/label 
+                          :id         :panner
+                          :size       [50 :by 50]
+                          :background :blue)]))))
 
 (defn -main [& args]
   (->
