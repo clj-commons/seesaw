e8914c7b70d82aedb95934726e4485b68f7a6c27
Dave Ray <daveray@gmail.com>
2012-02-27 20:54:11 -0500

    string-shape test

--- test/seesaw/test/graphics.clj
+++ test/seesaw/test/graphics.clj
@@ -128,6 +128,10 @@
       (expect (= [8.0 16.0 3.0 4.0 0.0 360.0]
                  [(.x s) (.y s) (.width s) (.height s) (.start s) (.extent s)])))))
 
+(describe string-shape
+  (it "creates a string shape"
+    (string-shape 1 2 "HI")))
+
 (describe stroke
   (it "creates a default stroke of width 1 with no args"
     (let [s (stroke)]
