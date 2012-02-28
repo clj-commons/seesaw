d5928f32c8f88ff7c0295eff42cf602d1403d766
Dave Ray <daveray@gmail.com>
2012-02-27 21:17:40 -0500

    Only run lazytest on test dir. Fix typo.

--- lazytest.sh
+++ lazytest.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 JAVA_OPTS="-XX:MaxPermSize=512m -Xmx512m"
-java $JAVA_OPTS -cp "src:test:classes:lib/*:lib/dev/*" lazytest.main src test
+java $JAVA_OPTS -cp "src:test:classes:lib/*:lib/dev/*" lazytest.main test
