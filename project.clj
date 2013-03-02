62a8928faed2be15a30a12653ed2792c8334551b
Dave Ray <daveray@gmail.com>
2013-03-01 18:26:08 -0800

    exclude org.clojure/clojure from j18n
    Now that 1.5.0's out, for issue #102, exclude org.clojure/clojure from
    the j18n dependencies which uses version ranges and can cause trouble.

--- project.clj
+++ project.clj
@@ -24,7 +24,8 @@
                  [com.miglayout/miglayout "3.7.4"]
                  [com.jgoodies/forms "1.2.1"]
                  [org.swinglabs.swingx/swingx-core "1.6.3"]
-                 [j18n "1.0.1"]
+                 ; exclude org.clojure/clojure to avoid its version range
+                 [j18n "1.0.1" :exclusions [org.clojure/clojure]]
                  [org.fife.ui/rsyntaxtextarea "2.0.4.1"]]
   :profiles { :dev {:dependencies [[com.stuartsierra/lazytest "1.1.2"]
                                   [lein-autodoc "0.9.0"]]}}
